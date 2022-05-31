a194587b33695c528d672ae8806958a47284cd72
mspielberg <9729801+mspielberg@users.noreply.github.com>
2022-05-31 09:24:19 -0700

    Hide children of [DvMod.HandBrake] transform if found

--- HandBrakeWheel.cs
+++ HandBrakeWheel.cs
@@ -80,7 +80,13 @@ namespace DvMod.HandBrake
             }
 
             if (car.transform.Find("[DvMod.HandBrake]") is Transform transform)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
                 return transform;
+            }
 
             if (wheelPositionMap.TryGetValue(car.carType, out var wheelPosition))
             {
